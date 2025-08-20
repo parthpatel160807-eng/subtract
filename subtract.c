862587868548308a5359b76396cccf8fef782329
parthpatel160807-eng <parthpatel160807@gmail.com>
2025-08-20 17:50:59 +0530

    Add files via upload

---
+++ subtract.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  printf("Enter two Numbers");
+  scanf("%d%d",&a,&b);
+  c=a-b;
+  printf("SUBTRACTION OF TWO NUMBERS IS %d and %d is %d",a,b,c);
+
+
+}
